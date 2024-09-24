56c0b4cf61434f83e7f6357c19a3e34d0765e79c
John Shehata <johnshehata11@gmail.com>
2024-09-24 23:37:34 +0300

    Create student and teacher page

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,22 @@
+import CalendarSide from "@/components/CalenderEvents/CalendarSide";
+import LessonSchedule from "@/components/LessonSchedule";
+
 const TeacherPage = () => {
   return (
-    <div>TeacherPage</div>
-  )
-}
+    <div className="flex flex-col xl:flex-row p-4 gap-4">
+      {/* Left Side */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white rounded-lg p-4 shadow-lg">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <LessonSchedule />
+        </div>
+      </div>
+      {/* Right Side */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <CalendarSide />
+      </div>
+    </div>
+  );
+};
 
-export default TeacherPage
+export default TeacherPage;
